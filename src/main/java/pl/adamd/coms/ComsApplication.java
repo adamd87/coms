fd4b9c71bc1ae5d374b83a428c54e18f6eeba68c
Adam <adam.drozdz1987@gmail.com>
2021-12-31 18:31:59 +0100

    Initial commit

---
+++ src/main/java/pl/adamd/coms/ComsApplication.java
@@ -0,0 +1,13 @@
+package pl.adamd.coms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComsApplication.class, args);
+	}
+
+}
